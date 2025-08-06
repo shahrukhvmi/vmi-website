89a3445fd51c0836e1bba3d018773151ddf523d6
usmanwarind46 <usmanwarind.vmi@gmail.com>
2025-08-06 17:25:19 +0500

    changes

--- components/portfolio/PortfolioSeoWrap.js
+++ components/portfolio/PortfolioSeoWrap.js
@@ -19,7 +19,7 @@ export default function PortfolioSeoWrap() {
   ];
 
   return (
-    <section className="portfolio-main-wrapper" id="portfolio">
+    <section className="portfolio-main-wrapper relative z-10" id="portfolio">
       <div className="w-6xl mx-auto max-container-width">
         <div className="portfolio-inner-heading text-center w-full">
           <h2 className="olivera-font">
